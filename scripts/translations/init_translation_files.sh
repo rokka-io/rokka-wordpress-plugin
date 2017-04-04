566bada862292407df7121c3fb6ebfa615ed5415
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-04-04 11:54:46 +0200

    create initial translation files

--- scripts/translations/init_translation_files.sh
+++ scripts/translations/init_translation_files.sh
@@ -1,3 +1,3 @@
 HERE=`dirname $0`
 ROOT="$HERE/../.."
-msginit -i "$ROOT/languages/rokka-image-cdn.pot" -o "$ROOT/languages/en_US.po" -l de_CH
+msginit -i "$ROOT/languages/rokka-image-cdn.pot" -o "$ROOT/languages/de_CH.po" -l de_CH
