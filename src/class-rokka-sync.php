61b43cee76b54a8184fb514b1a94aa01e8399a4e
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-04-09 10:07:54 +0200

    remove unused filter

--- src/class-rokka-sync.php
+++ src/class-rokka-sync.php
@@ -34,7 +34,6 @@ class Rokka_Sync {
 		add_action( 'add_attachment', array( $this, 'rokka_upload' ), 10, 1 );
 		add_filter( 'update_attached_file', array( $this, 'rokka_update' ), 10, 2 );
 		add_action( 'delete_attachment', array( $this, 'rokka_delete' ), 10, 1 );
-		add_filter( 'wp_save_image_editor_file', array( $this, 'rokka_save_image_editor_file' ), 10, 5 );
 
 		add_action( 'wp_ajax_rokka_upload_image', array( $this, 'ajax_rokka_upload_image' ) );
 		add_action( 'wp_ajax_rokka_create_stacks', array( $this, 'ajax_rokka_create_stacks' ) );
@@ -84,26 +83,6 @@ class Rokka_Sync {
 		}
 
 		return $this->rokka_helper->delete_image_from_rokka( $post_id );
-	}
-
-	/**
-	 * Filter whether to skip saving the image file.
-	 *
-	 * Returning a non-null value will short-circuit the save method,
-	 * returning that value instead.
-	 *
-	 * @since 3.5.0
-	 *
-	 * @param mixed           $override Value to return instead of saving. Default null.
-	 * @param string          $filename Name of the file to be saved.
-	 * @param WP_Image_Editor $image WP_Image_Editor instance.
-	 * @param string          $mime_type Image mime type.
-	 * @param int             $post_id Post ID.
-	 *
-	 * @return null|bool
-	 */
-	public function rokka_save_image_editor_file( $override, $filename, $image, $mime_type, $post_id ) {
-		return $override;
 	}
 
 	/**
