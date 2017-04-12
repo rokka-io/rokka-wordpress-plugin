65bb8373478a763abfe5fe61e18d40af54773ee4
philorkill <philou.savary@gmail.com>
2017-04-12 13:31:43 +0200

    installation script added

---
+++ scripts/install-wp-plugin.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+# Let's begin...
+echo ".........................................."
+echo
+echo "Preparing to install WordPress plugin"
+echo
+echo ".........................................."
+echo
+
+echo "Changing to plugin path"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+cd ..
+pwd
+
+echo "Installing composer packages"
+curl -s https://getcomposer.org/installer | php
+php composer.phar install --no-dev
+
+echo "Installing node modules"
+
+npm install --loglevel error
+echo "Running gulp deploy task"
+node_modules/.bin/gulp deploy
+
+#echo "Compile translation files"
+#for file in `find "languages" -name "*.po"` ; do msgfmt -o ${file/.po/.mo} $file ; done
+
+
+echo "*** Installation of Rokka Plugin complete ***"
