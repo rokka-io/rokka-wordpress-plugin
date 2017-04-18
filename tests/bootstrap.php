f185255a053174246d5143674e0b4685dbb1607e
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-04-18 16:00:44 +0200

    implement basic unit test setup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,25 @@
 <?php
-
-require_once dirname( __FILE__ ) . '/../web/wp-unittest-config.php';
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package rokka-image-cdn
+ */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
-function _manually_switch_theme() {
-	switch_theme( 'nebis' );
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/rokka-image-cdn.php';
 }
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-tests_add_filter( 'plugins_loaded', '_manually_switch_theme' );
-
-// autoload timber plugin dependencies
-if ( file_exists($composer_autoload = __DIR__ . '/../web/content/plugins/timber-library/vendor/autoload.php') ) {
-	require_once($composer_autoload);
-}
-
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
--- tests/test-plugin-functions.php
+++ tests/test-plugin-functions.php
@@ -1,8 +1,27 @@
 <?php
 
 class PluginFunctionsTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		parent::setUp();
+		$this->_plugin_dir = dirname( dirname( __FILE__ ) );
+		$this->attachment_bigger_than_biggest_size_id = $this->factory->attachment->create_upload_object( $this->_plugin_dir . '/tests/images/2000x1500.png', 0 );
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+		$upload_dir = wp_get_upload_dir();
+		//rmdir($upload_dir['basedir']);
+		exec(sprintf("rm -rf %s", escapeshellarg($upload_dir['basedir'])));
+		exec(sprintf("mkdir -p %s", escapeshellarg($upload_dir['basedir'])));
+	}
+
 	function test_get_plugin_version() {
 		$version = '1.0.0';
 		$this->assertEquals( '1.0.0', $version );
+	}
+
+	public function test_date_query_before_array() {
+		echo wp_get_attachment_url( $this->attachment_bigger_than_biggest_size_id );
 	}
 }
