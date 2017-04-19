3b5087c49a368d48c957585d2d90507b418c7c91
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-04-19 16:37:35 +0200

    improve upload/delete buttons in media management

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function () {
-	jQuery('#rokka-delete-image').click( function(e) {
-		if ( confirm( rokkaAdmin.labels.deleteImageConfirm ) !== true) {
+	jQuery('.rokka-delete-image').click( function(e) {
+		if ( confirm( rokkaAdmin.labels.deleteImageConfirm ) !== true ) {
 			e.preventDefault();
 		}
 	});
