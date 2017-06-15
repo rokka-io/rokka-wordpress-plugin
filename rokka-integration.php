dc71f42db011547a3d8b25f66bf54a99f02ae2de
Stefanie Taepke <stefanie.taepke@liip.ch>
2017-06-15 17:22:39 +0200

    refactor requirement-style

--- rokka-integration.php
+++ rokka-integration.php
@@ -24,16 +24,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'ROKKA_PLUGIN_PATH', plugins_url( '', __FILE__ ) );
 
 // Load plugin class files
-require_once( 'src/class-rokka-integration.php' );
-require_once( 'src/class-rokka-integration-settings.php' );
-require_once( 'src/class-rokka-helper.php' );
-require_once( 'src/class-rokka-media-management.php' );
-require_once( 'src/class-rokka-filter-url.php' );
-require_once( 'src/class-rokka-filter-content.php' );
-require_once( 'src/RokkaCliCommand/class-rokka-wp-cli-command.php' );
+require_once( dirname( __FILE__ ) . '/src/class-rokka-integration.php' );
+require_once( dirname( __FILE__ ) . '/src/class-rokka-integration-settings.php' );
+require_once( dirname( __FILE__ ) . '/src/class-rokka-helper.php' );
+require_once( dirname( __FILE__ ) . '/src/class-rokka-media-management.php' );
+require_once( dirname( __FILE__ ) . '/src/class-rokka-filter-url.php' );
+require_once( dirname( __FILE__ ) . '/src/class-rokka-filter-content.php' );
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	require_once( dirname( __FILE__ ) . '/src/RokkaCliCommand/class-rokka-wp-cli-command.php' );
+}
 
 //add vendor library
-require_once( 'vendor/autoload.php' );
+require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
 
 /**
  * Returns the main instance of Rokka_Integration to prevent the need to use globals.
@@ -61,7 +63,7 @@ function rokka_integration() {
 		}
 	}
 
-	if ( class_exists( 'WP_CLI_Command' ) ) {
+	if ( defined( 'WP_CLI' ) && WP_CLI ) {
 		\WP_CLI::add_command(
 			'rokka',
 			'Rokka_Wp_Cli_Command'
