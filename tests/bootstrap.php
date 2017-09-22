b3e8278864ae09663234767dd79808d40eeae89e
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-09-22 15:10:02 +0200

    use renamed plugin

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * PHPUnit bootstrap file
  *
- * @package rokka-image-cdn
+ * @package rokka-integration
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
@@ -17,7 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/rokka-image-cdn.php';
+	require dirname( dirname( __FILE__ ) ) . '/rokka-integration.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
