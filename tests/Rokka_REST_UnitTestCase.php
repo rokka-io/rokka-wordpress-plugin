3d7108019e1e860a15e5613adbaf4063737ae0c9
Juerg Hunziker <juerg.hunziker@liip.ch>
2018-01-30 09:28:35 +0100

    extract rest image upload

--- tests/Rokka_REST_UnitTestCase.php
+++ tests/Rokka_REST_UnitTestCase.php
@@ -7,13 +7,17 @@
  */
 
 class Rokka_REST_UnitTestCase extends Rokka_UnitTestCase {
+	/**
+	 * @var \WP_REST_Server
+	 */
 	protected $server;
+
 	/**
 	 * Setup our test server.
 	 */
 	public function setUp() {
 		parent::setUp();
-		/** @var WP_REST_Server $wp_rest_server */
+		/** @var \WP_REST_Server $wp_rest_server */
 		global $wp_rest_server;
 		$this->server = $wp_rest_server = new \WP_REST_Server;
 		do_action( 'rest_api_init' );
@@ -25,5 +29,23 @@ class Rokka_REST_UnitTestCase extends Rokka_UnitTestCase {
 		parent::tearDown();
 		global $wp_rest_server;
 		$wp_rest_server = null;
+	}
+
+	/**
+	 * Uploads image via rest and returns response.
+	 *
+	 * @param string $image_name Name of image which should be uploaded.
+	 * @return WP_REST_Response
+	 */
+	public function upload_image_via_rest( $image_name ) {
+		$file = file_get_contents( $this->images_dir . $image_name );
+		// create admin user
+		$user_id = $this->factory->user->create( array( 'role' => 'administrator' ) );
+		wp_set_current_user( $user_id );
+		$request  = new \WP_REST_Request( 'POST', '/wp/v2/media' );
+		$request->set_body( $file );
+		$request->set_header( 'content-type', 'image/png' );
+		$request->set_header( 'Content-Disposition', 'attachment; filename="' . $image_name . '"' );
+		return $this->server->dispatch( $request );
 	}
 }
--- tests/test-rokka-rest.php
+++ tests/test-rokka-rest.php
@@ -3,15 +3,7 @@
 class RokkaRestTest extends Rokka_REST_UnitTestCase {
 	public function test_add_image_via_rest_without_rokka() {
 		$image_name = '2000x1500.png';
-		$file = file_get_contents( $this->images_dir . $image_name );
-		// create admin user
-		$user_id = $this->factory->user->create( array( 'role' => 'administrator' ) );
-		wp_set_current_user( $user_id );
-		$request  = new \WP_REST_Request( 'POST', '/wp/v2/media' );
-		$request->set_body( $file );
-		$request->set_header( 'content-type', 'image/png' );
-		$request->set_header( 'Content-Disposition', 'attachment; filename="' . $image_name . '"' );
-		$response = $this->server->dispatch( $request );
+		$response = $this->upload_image_via_rest( $image_name );
 		// should have status 201 Created
 		$this->assertEquals( 201, $response->get_status() );
 		$data = $response->get_data();
@@ -22,15 +14,7 @@ class RokkaRestTest extends Rokka_REST_UnitTestCase {
 	public function test_add_image_via_rest() {
 		$this->enable_rokka();
 		$image_name = '2000x1500.png';
-		$file = file_get_contents( $this->images_dir . $image_name );
-		// create admin user
-		$user_id = $this->factory->user->create( array( 'role' => 'administrator' ) );
-		wp_set_current_user( $user_id );
-		$request  = new \WP_REST_Request( 'POST', '/wp/v2/media' );
-		$request->set_body( $file );
-		$request->set_header( 'content-type', 'image/png' );
-		$request->set_header( 'Content-Disposition', 'attachment; filename="' . $image_name . '"' );
-		$response = $this->server->dispatch( $request );
+		$response = $this->upload_image_via_rest( $image_name );
 		// should have status 201 Created
 		$this->assertEquals( 201, $response->get_status() );
 		$data = $response->get_data();
