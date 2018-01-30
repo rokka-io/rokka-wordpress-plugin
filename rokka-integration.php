1aabe3b3ddb5c7910ba01523bdffc6b3ce2e4913
Juerg Hunziker <juerg.hunziker@liip.ch>
2018-01-30 08:49:25 +0100

    move main class to main plugin file

--- rokka-integration.php
+++ rokka-integration.php
@@ -21,58 +21,253 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ROKKA_PLUGIN_PATH', plugins_url( '', __FILE__ ) );
+if ( ! class_exists( 'Rokka_Integration' ) ) :
 
-// Load plugin class files
-require_once( dirname( __FILE__ ) . '/src/class-rokka-integration.php' );
-require_once( dirname( __FILE__ ) . '/src/class-rokka-integration-settings.php' );
-require_once( dirname( __FILE__ ) . '/src/class-rokka-helper.php' );
-require_once( dirname( __FILE__ ) . '/src/class-rokka-media-management.php' );
-require_once( dirname( __FILE__ ) . '/src/class-rokka-rest.php' );
-require_once( dirname( __FILE__ ) . '/src/class-rokka-filter-url.php' );
-require_once( dirname( __FILE__ ) . '/src/class-rokka-filter-content.php' );
-if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	require_once( dirname( __FILE__ ) . '/src/cli-command/class-rokka-wp-cli-command.php' );
-}
+	/**
+	 * Class Rokka_Integration
+	 */
+	class Rokka_Integration {
 
-//add vendor library
-require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
+		/**
+		 * Rokka_Integration instance.
+		 *
+		 * @var Rokka_Integration
+		 */
+		protected static $_instance = null;
 
-/**
- * Returns the main instance of Rokka_Integration to prevent the need to use globals.
- *
- * @return Rokka_Integration Rokka_Integration instance
- */
-function rokka_integration() {
-	$rokka_helper = new Rokka_Helper();
-	$instance = Rokka_Integration::instance( __FILE__, '1.2.3', $rokka_helper );
+		/**
+		 * Rokka_Helper instance.
+		 *
+		 * @var Rokka_Helper
+		 */
+		public $rokka_helper = null;
 
-	if ( $rokka_helper->is_rokka_enabled() ) {
-		new Rokka_Filter_Url( $rokka_helper );
-		new Rokka_Rest( $rokka_helper );
-		if ( ! is_admin() && $rokka_helper->is_output_parsing_enabled() ) {
-			new Rokka_Filter_Content( $rokka_helper );
+		/**
+		 * Rokka_Integration_Settings instance.
+		 *
+		 * @var Rokka_Integration_Settings
+		 */
+		public $settings = null;
+
+		/**
+		 * The plugin version number.
+		 *
+		 * @var string
+		 */
+		public $_version = '2.0.0';
+
+		/**
+		 * The token.
+		 *
+		 * @var string
+		 */
+		public $_token = 'rokka-integration';
+
+		/**
+		 * The main plugin file.
+		 *
+		 * @var string
+		 */
+		public $file;
+
+		/**
+		 * The main plugin directory.
+		 *
+		 * @var string
+		 */
+		public $dir;
+
+		/**
+		 * The plugin assets directory.
+		 *
+		 * @var string
+		 */
+		public $assets_dir;
+
+		/**
+		 * The plugin assets URL.
+		 *
+		 * @var string
+		 */
+		public $assets_url;
+
+		/**
+		 * Rokka_Integration constructor.
+		 */
+		public function __construct() {
+			$this->init_plugin_environment();
+			$this->includes();
+			$this->init_hooks();
+			$this->init_plugin();
 		}
+
+		/**
+		 * Initializes plugin environment variables
+		 */
+		protected function init_plugin_environment() {
+			// Load plugin environment variables
+			$this->file       = __FILE__;
+			$this->dir        = dirname( $this->file );
+			$this->assets_dir = trailingslashit( $this->dir ) . 'assets';
+			$this->assets_url = esc_url( trailingslashit( plugins_url( '/assets/', $this->file ) ) );
+		}
+
+		/**
+		 * Include required core files used in admin and on the frontend.
+		 */
+		public function includes() {
+			// Load plugin class files
+			require_once( dirname( __FILE__ ) . '/src/class-rokka-integration-settings.php' );
+			require_once( dirname( __FILE__ ) . '/src/class-rokka-helper.php' );
+			require_once( dirname( __FILE__ ) . '/src/class-rokka-media-management.php' );
+			require_once( dirname( __FILE__ ) . '/src/class-rokka-rest.php' );
+			require_once( dirname( __FILE__ ) . '/src/class-rokka-filter-url.php' );
+			require_once( dirname( __FILE__ ) . '/src/class-rokka-filter-content.php' );
+			if ( defined( 'WP_CLI' ) && WP_CLI ) {
+				require_once( dirname( __FILE__ ) . '/src/cli-command/class-rokka-wp-cli-command.php' );
+			}
+
+			// add vendor library
+			require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
+		}
+
+		/**
+		 * Initializes hooks.
+		 */
+		protected function init_hooks() {
+			register_activation_hook( $this->file, array( $this, 'install' ) );
+
+			// Load admin JS & CSS
+			add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_assets' ), 10, 1 );
+
+			// Load textdomain
+			add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
+		}
+
+		protected function init_plugin() {
+			$this->rokka_helper = new Rokka_Helper();
+
+			if ( $this->rokka_helper->is_rokka_enabled() ) {
+				new Rokka_Filter_Url( $this->rokka_helper );
+				new Rokka_Rest( $this->rokka_helper );
+				if ( ! is_admin() && $this->rokka_helper->is_output_parsing_enabled() ) {
+					new Rokka_Filter_Content( $this->rokka_helper );
+				}
+			}
+
+			if ( is_admin() ) {
+				if ( $this->rokka_helper->is_rokka_enabled() ) {
+					new Rokka_Media_Management( $this->rokka_helper );
+				}
+
+				if ( is_null( $this->settings ) ) {
+					$this->settings = Rokka_Integration_Settings::instance( $this );
+				}
+			}
+
+			if ( defined( 'WP_CLI' ) && WP_CLI ) {
+				WP_CLI::add_command(
+					'rokka',
+					'Rokka_Wp_Cli_Command'
+				);
+			}
+		}
+
+		/**
+		 * Sets the rokka helper. (Should only be used in unit tests to reinitialize rokka heler)
+		 *
+		 * @param $rokka_helper
+		 */
+		public function set_rokka_helper( $rokka_helper ) {
+			$this->rokka_helper = $rokka_helper;
+		}
+
+		/**
+		 * Load admin assets.
+		 *
+		 * @param string $hook Current page hook.
+		 */
+		public function admin_enqueue_assets( $hook ) {
+			wp_register_style( $this->_token . '-admin', esc_url( $this->assets_url ) . 'css/admin.css', array(), $this->_version );
+			wp_enqueue_style( $this->_token . '-admin' );
+
+			wp_register_script( $this->_token . '-admin', esc_url( $this->assets_url ) . 'js/admin.min.js', array( 'jquery' ), $this->_version, true );
+			wp_enqueue_script( $this->_token . '-admin' );
+
+			$rokka_admin = array(
+				'labels' => array(
+					'deleteImageConfirm' => esc_html__( 'Do you really want to delete this image from rokka? Please be aware that all stored meta information (eg. subject area) will be deleted as well.', 'rokka-integration' ),
+				),
+			);
+			wp_localize_script( $this->_token . '-admin', 'rokkaAdmin', $rokka_admin );
+
+			wp_register_script( $this->_token . '-subject-area', esc_url( $this->assets_url ) . 'js/rokka-subject-area.min.js', array( 'jquery', 'imgareaselect' ), $this->_version, false );
+			wp_enqueue_script( $this->_token . '-subject-area' );
+
+			// Load only on rokka settings page
+			if ( 'settings_page_' . $this->settings->menu_slug === $hook ) {
+				wp_register_script( $this->_token . '-settings-js', $this->assets_url . 'js/settings.min.js', array( 'jquery' ), $this->_version, true );
+				wp_enqueue_script( $this->_token . '-settings-js' );
+
+				// add progessbar for mass upload
+				wp_enqueue_script( 'jquery-ui-progressbar' );
+				wp_register_style( $this->_token . '-jquery-ui', esc_url( $this->assets_url ) . '/css/jquery-ui.min.css', array(), '1.12.1' );
+				wp_enqueue_style( $this->_token . '-jquery-ui' );
+			}
+		}
+
+		/**
+		 * Load plugin textdomain
+		 */
+		public function load_plugin_textdomain() {
+			$domain = 'rokka-integration'; // textdomain can't be stored in class variable since it must be a single string literal
+			load_plugin_textdomain( $domain, false, dirname( plugin_basename( $this->file ) ) . '/languages/' );
+		}
+
+		/**
+		 * Main Rokka_Integration Instance
+		 * Ensures only one instance of Rokka_Integration is loaded or can be loaded.
+		 *
+		 * @return Rokka_Integration Rokka_Integration instance
+		 */
+		public static function instance() {
+			if ( is_null( self::$_instance ) ) {
+				self::$_instance = new self();
+			}
+
+			return self::$_instance;
+		}
+
+		/**
+		 * Cloning is forbidden.
+		 */
+		public function __clone() {
+			_doing_it_wrong( __FUNCTION__, esc_html__( 'Cheatin&#8217; huh?' ), esc_attr( $this->_version ) );
+		}
+
+		/**
+		 * Unserializing instances of this class is forbidden.
+		 */
+		public function __wakeup() {
+			_doing_it_wrong( __FUNCTION__, esc_html__( 'Cheatin&#8217; huh?' ), esc_attr( $this->_version ) );
+		}
+
+		/**
+		 * Installation. Runs on activation.
+		 */
+		public function install() {
+			$this->_log_version_number();
+		}
+
+		/**
+		 * Log the plugin version number in database.
+		 */
+		private function _log_version_number() {
+			update_option( $this->_token . '_version', $this->_version );
+		}
+
 	}
 
-	if ( is_admin() ) {
-		if ( $rokka_helper->is_rokka_enabled() ) {
-			new Rokka_Media_Management( $rokka_helper );
-		}
+endif ;
 
-		if ( is_null( $instance->settings ) ) {
-			$instance->settings = Rokka_Integration_Settings::instance( $instance, $rokka_helper );
-		}
-	}
-
-	if ( defined( 'WP_CLI' ) && WP_CLI ) {
-		WP_CLI::add_command(
-			'rokka',
-			'Rokka_Wp_Cli_Command'
-		);
-	}
-
-	return $instance;
-}
-
-rokka_integration();
+Rokka_Integration::instance();
