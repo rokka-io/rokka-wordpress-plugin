c2c9b74292223b919cdf7b1b3ff1f0369fc3e32c
Juerg Hunziker <juerg.hunziker@liip.ch>
2018-02-01 10:29:08 +0100

    tested up to 4.9.2

--- rokka-integration.php
+++ rokka-integration.php
@@ -9,7 +9,7 @@
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Requires at least: 4.0
- * Tested up to: 4.8.2
+ * Tested up to: 4.9.2
  *
  * Text Domain: rokka-integration
  * Domain Path: /languages/
