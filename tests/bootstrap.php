47f5f1c7cac5e0e82633055d0e940e3b6795b4dd
Juerg Hunziker <juerg.hunziker@liip.ch>
2018-06-02 15:10:39 +0200

    install wordpress core in specific version folders

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,17 @@
  * @package rokka-integration
  */
 
+$_wp_version = getenv( 'WP_VERSION' );
+if ( ! $_wp_version ) {
+	$_wp_version = 'latest';
+}
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = '/tmp/wordpress-tests-lib/' . $_wp_version;
 }
+
+echo "Running tests with WordPress " . $_wp_version . PHP_EOL;
 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
