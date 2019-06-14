b9474ad4745f0502b61d582889e19ebd7ebd5b74
Juerg Hunziker <juerg.hunziker@liip.ch>
2019-06-14 15:57:27 +0200

    rename variables

--- tests/test-rokka-base.php
+++ tests/test-rokka-base.php
@@ -10,12 +10,12 @@ class Rokka_Base_Test extends Rokka_UnitTestCase {
 	 */
 	public function test_check_version() {
 		$rokka_integration_instance = Rokka_Integration::instance();
-		update_option( 'rokka-integration_version', ( (float) $rokka_integration_instance->_version - 1 ) );
+		update_option( 'rokka-integration_version', ( (float) $rokka_integration_instance->version - 1 ) );
 		$rokka_integration_instance->check_version();
 		// rokka-integration_updated action should have been called 2 times (1. first load of plugin when starting unit test 2. manual change of version in this unit test)
 		$this->assertEquals( 2, did_action( 'rokka-integration_updated' ) );
 
-		update_option( 'rokka-integration_version', $rokka_integration_instance->_version );
+		update_option( 'rokka-integration_version', $rokka_integration_instance->version );
 		$rokka_integration_instance->check_version();
 		// rokka-integration_updated action should not have been called again since the version number already matched.
 		$this->assertEquals( 2, did_action( 'rokka-integration_updated' ) );
