2501bad49e954dc7fb80d5e35a9675a7de6412d8
Juerg Hunziker <juerg.hunziker@liip.ch>
2021-09-22 11:58:23 +0200

    fix user argument

--- scripts/install-wp-tests-clone.sh
+++ scripts/install-wp-tests-clone.sh
@@ -3,4 +3,4 @@
 DB_HOST=$1
 
 mysql --host $DB_HOST --port 3306 -uroot -proot -e "SHOW DATABASES"
-mysql --host $DB_HOST --port 3306 --user "root" --password "root" -e "drop database if exists test"
+mysql --host $DB_HOST --port 3306 --user="root" --password="root" -e "drop database if exists test"
