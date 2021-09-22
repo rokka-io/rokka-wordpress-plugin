9ee9922b64fd906cb5a2274a4ed4f0ab8d8ddb5d
Juerg Hunziker <juerg.hunziker@liip.ch>
2021-09-22 11:56:10 +0200

    add check mysql script

---
+++ scripts/install-wp-tests-clone.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+DB_HOST=$1
+
+mysql --host $DB_HOST --port 3306 -uroot -proot -e "SHOW DATABASES"
+mysql --host $DB_HOST --port 3306 --user "root" --password "root" -e "drop database if exists test"
