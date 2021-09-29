342c7e851a3b4d94a46644ab10dfacbda12aed4d
Juerg Hunziker <juerg.hunziker@liip.ch>
2021-09-29 15:53:52 +0200

    use wp-env to run tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,20 +5,36 @@
  * @package rokka-integration
  */
 
-$_wp_version = getenv( 'WP_VERSION' );
-if ( ! $_wp_version ) {
-	$_wp_version = 'latest';
-}
+// Require composer dependencies.
+require_once dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php';
 
+// Determine the tests directory (from a WP dev checkout).
+// Try the WP_TESTS_DIR environment variable first.
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+// Next, try the WP_PHPUNIT composer package.
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib/' . $_wp_version;
+	$_tests_dir = getenv( 'WP_PHPUNIT__DIR' );
 }
 
-echo "Running tests with WordPress " . $_wp_version . PHP_EOL;
+// See if we're installed inside an existing WP dev instance.
+if ( ! $_tests_dir ) {
+	$_try_tests_dir = dirname( __FILE__ ) . '/../../../../../tests/phpunit';
+	if ( file_exists( $_try_tests_dir . '/includes/functions.php' ) ) {
+		$_tests_dir = $_try_tests_dir;
+	}
+}
+// Fallback.
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
+
+// Do not try to load JavaScript files from an external URL - this takes a
+// while.
+define( 'GUTENBERG_LOAD_VENDOR_SCRIPTS', false );
 
 /**
  * Manually load the plugin being tested.
